347eb7999b91c553ea495c6ca791c760f1e7812c
johnelmer <johnelmer_loretizo@yahoo.com>
2018-11-14 23:01:15 -0800

    feat(main.py): Added new data munging methods

--- main.py
+++ main.py
@@ -4,9 +4,33 @@ import pandas as pd
 # X = train
 def clean_data(df):
     df = df.drop('Cabin', axis=1)
+    df = df.drop('Ticket', axis=1)
+    df = df.drop('Name', axis=1)
+    df['Sex'].replace({'male': 0, 'female': 1},inplace=True)
     df['Embarked'] = df['Embarked'].fillna('S')
     for pclass in df['Pclass'].unique():
         df.loc[(df['Pclass'] == pclass) & (df['Age'].isnull())] = df.groupby('Pclass')['Age'].mean().loc[pclass]
+
+    return df
+
+def one_hot_encode(df):
+    df['Embarked_S'] =0
+    df['Embarked_C'] =0
+    df['Embarked_Q'] =0
+    df.loc[df['Embarked'] == 'S', 'Embarked_S'] = 1
+    df.loc[df['Embarked'] == 'C', 'Embarked_C'] = 1
+    df.loc[df['Embarked'] == 'Q', 'Embarked_Q'] = 1
+
+    return df
+
+def flag_infant(df):
+    df['infant'] = df['Age'] < 10
+    df = df.drop('Age', axis=1)
+
+    return df
+
+def extract_title(df):
+    df['Name'].str.split(',').apply(lambda x: x[1]).str.split('.').apply(lambda x: x[0]).unique()
 
     return df
 
